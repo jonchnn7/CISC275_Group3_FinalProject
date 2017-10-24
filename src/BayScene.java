beaf33eae6413a69a80df9ecc770443d2b833c79
Scott Fones <scott.fones@gmail.com>
2017-10-23 21:59:56 -0400

    bug fixes and performance improvements

--- src/BayScene.java
+++ src/BayScene.java
@@ -21,18 +21,18 @@ public class BayScene extends Scene {
 		
 		for (int j=0; j<5; j++) {
 			int length = rand_gen.nextInt(20) + 15;
-			this.scene_items.add(new AlphaFish(0, 
+			this.scene_items.add(new AlphaFish(0-length, 
 											   j*140 + 10,
 											   length,
 											   length/2,
 											   j+1,
 											   true));
-			this.scene_items.add(new AlphaFish(this.scene_width, 
-					   j*140 + 10,
-					   length,
-					   length/2,
-					   -1*j-1,
-					   false));
+			this.scene_items.add(new AlphaFish(this.scene_width+length, 
+											   j*140 + 10,
+											   length,
+											   length/2,
+											   -1*j-1,
+											   false));
 		
 		Collections.sort(scene_items);	
 		}
@@ -43,17 +43,17 @@ public class BayScene extends Scene {
 		
 		// Generate new fish on ~4% of calls
 		if (rand_gen.nextInt(25) == 1) {
-			this.scene_items.add(new AlphaFish(0, 
+			this.scene_items.add(new AlphaFish(0-length, 
 					   						   rand_gen.nextInt(this.scene_height),
 					   						   length,
 					   						   length/2,
-					   						   rand_gen.nextInt(5)+5,
+					   						   rand_gen.nextInt(10)+5,
 					   						   true));
-			this.scene_items.add(new AlphaFish(this.scene_width, 
+			this.scene_items.add(new AlphaFish(this.scene_width+length, 
 					               			   rand_gen.nextInt(this.scene_height),
 					               			   length,
 					               			   length/2,
-					               			   -1*rand_gen.nextInt(5)+5,
+					               			   -1*rand_gen.nextInt(10)-5,
 					               			   false));		
 		}
 		
@@ -66,10 +66,10 @@ public class BayScene extends Scene {
 	private void removeFish() {
 		for (Iterator<SceneObject> iterator = scene_items.iterator(); iterator.hasNext();) {
 			AlphaFish fish = (AlphaFish)iterator.next();
-			if ( fish.left_to_right && fish.item_x >= this.scene_width ) {
+			if ( fish.left_to_right && fish.item_x >= (this.scene_width+fish.item_width) ) {
 				iterator.remove();
 			
-			} else if ( !fish.left_to_right && fish.item_x <= 0 ) { 
+			} else if ( !fish.left_to_right && fish.item_x <= (0-fish.item_width) ) { 
 				iterator.remove();
 			}
 		}
