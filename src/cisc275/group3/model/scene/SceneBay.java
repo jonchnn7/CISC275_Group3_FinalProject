65deb1b86f3135eec530ea92c9e5a38a3a634b98
Scott Fones <scott.fones@gmail.com>
2017-11-06 14:59:09 -0500

    created ConstructFish.java interface to be responsible for returning fish objects.

--- src/cisc275/group3/model/scene/SceneBay.java
+++ src/cisc275/group3/model/scene/SceneBay.java
@@ -7,43 +7,16 @@ import java.util.Iterator;
 import cisc275.group3.model.sceneobject.BetaFish;
 import cisc275.group3.model.sceneobject.NavObject;
 import cisc275.group3.model.sceneobject.SceneObject;
+import cisc275.group3.utility.ConstructFish;
 import cisc275.group3.utility.ObjectId;
 import cisc275.group3.utility.SceneId;
 
 /**
  * Bay game scene. 
  * <p>
- * BayScene.java
+ * SceneBay.java
  */
-public class SceneBay extends Scene implements PropertyScored, PropertyTimed {
-  
-  // Fish File Locations
-  private static final String[] FISH_LEFT_FILES = 
-	    {"img/fish_left_1.png", "img/fish_left_2.png", "img/fish_left_3.png"};
-  private static final String[] FISH_RIGHT_FILES = 
-	    {"img/fish_right_1.png", "img/fish_right_2.png", "img/fish_right_3.png"};
-  
-  // Fish Names
-  private static final String[] FISH_LEFT_NAMES = 
-    {"Butterflyfish", "Rainbow Cichlid", "Goldfish"};
-  private static final String[] FISH_RIGHT_NAMES = 
-	{"Angelfish", "Threadfin Butterflyfish", "Sergeant Major"};
-  
-  // Fish Dimensions
-  private static final int[] FISH_LEFT_LENGTH = {125, 100, 80};
-  private static final int[] FISH_RIGHT_LENGTH = {100, 120, 85};
-  
-  // Fish Aspect Ratios
-  private static final double[] FISH_LEFT_AR = {0.6367, 0.7898, 0.5828};
-  private static final double[] FISH_RIGHT_AR = {0.7047, 0.7453, 0.6};
-  
-  // Fish ID Numbers
-  private static final int[] FISH_LEFT_ID = {1, 2, 3};
-  private static final int[] FISH_RIGHT_ID = {4, 5, 6};
-  
-  // Fish x-axis Speed
-  private static final int[] FISH_SPEED = {19, 17, 13};
-  
+public class SceneBay extends Scene implements ConstructFish, PropertyScored, PropertyTimed {
 
   public SceneBay(SceneId mani, boolean click, boolean vis) {
     super(mani, click, vis);
@@ -72,28 +45,19 @@ public class SceneBay extends Scene implements PropertyScored, PropertyTimed {
     for (int i=0; i<5; i++) {
       int fishType = randGen.nextInt(3);
       
-      // Create Left Fish ID
-      ObjectId leftFishId = createLeftFishId(randGen.nextInt(20)-10, fishType);
+      // Add Left Fish
+      sceneItems.add(ConstructFish.constructLeftFish(
+                     randGen.nextInt(20)-10, // depth
+                     fishType,
+                     manifest.getWidth()+randGen.nextInt(500), // x location
+                     i*140 + manifest.getStartY() + 10)); // y location
       
-      // Create Right Fish ID
-      ObjectId rightFishId = createRightFishId(randGen.nextInt(20)-10, fishType);
-      
-      // Add Left Fish to Scene
-      sceneItems.add(new BetaFish(
-        leftFishId, // fish id
-        manifest.getWidth()+randGen.nextInt(500), // x location
-        i*140 + manifest.getStartY() + 10, // y location
-        FISH_SPEED[fishType]+randGen.nextInt(4), // speed x
-        0, // speed y
-        true)); // left moving fish?
-      
-      sceneItems.add(new BetaFish(
-        rightFishId, // fish id
-        0 - randGen.nextInt(500), // x location
-        i*140 + manifest.getStartY() + 10, // y location
-        FISH_SPEED[fishType], // speed x
-        0, // speed y
-        false)); // left moving fish?
+      // Add Right Fish
+      sceneItems.add(ConstructFish.constructRightFish(
+                     randGen.nextInt(20)-10, // depth
+                     fishType,
+                     0 - randGen.nextInt(500), // x location
+                     i*140 + manifest.getStartY() + 10)); // y location
     }
     Collections.sort(sceneItems); // sort by depth
   }
@@ -110,22 +74,18 @@ public class SceneBay extends Scene implements PropertyScored, PropertyTimed {
   public void update() {
     // Generate new fish on ~4% of calls
     if (randGen.nextInt(100) <= 4) {
-      sceneItems.add(new BetaFish(
-        createLeftFishId(randGen.nextInt(20)-10, randGen.nextInt(3)), // fish id	
-        manifest.getWidth() + randGen.nextInt(75), // x location
-        randGen.nextDouble()*manifest.getHeight() + manifest.getStartY(), // y location
-        15, // speed x
-        0, // speed y
-        true)); // moving left?
+      sceneItems.add(ConstructFish.constructLeftFish(
+                     randGen.nextInt(20)-10, // depth
+                     randGen.nextInt(3),
+                     manifest.getWidth()+randGen.nextInt(500), // x location
+                     randGen.nextDouble()*manifest.getHeight() + manifest.getStartY())); // y location
       
-      sceneItems.add(new BetaFish(
- 	    createRightFishId(randGen.nextInt(20)-10, randGen.nextInt(3)), // fish id	
-        0 - randGen.nextInt(75), // x location
-    	randGen.nextDouble()*manifest.getHeight() + manifest.getStartY(), // y location
-    	15, // speed x
-    	0, // speed y
-    	false)); // moving left?
-    }	
+      sceneItems.add(ConstructFish.constructRightFish(
+                     randGen.nextInt(20)-10, // depth
+                     randGen.nextInt(3),
+                     0 - randGen.nextInt(75), // x location
+                     randGen.nextDouble()*manifest.getHeight() + manifest.getStartY())); // y location
+    }
     
     // Move Fish
     for (SceneObject fish : sceneItems) {
@@ -137,44 +97,6 @@ public class SceneBay extends Scene implements PropertyScored, PropertyTimed {
     
     // Sort by depth
     Collections.sort(sceneItems);
-  }
-  
-  /**
-   * Utility function to create a left fish id's
-   * @param 	dpth		fish depth
-   * @param		fishType	int for array selection
-   * @return	leftFishId	returns created ObjectId
-   */
-  private ObjectId createLeftFishId(int dpth, int fishType) {
-	// Create Left Fish ID
-    ObjectId leftFishId = new ObjectId(
-      dpth, // depth
-      (int)(FISH_LEFT_LENGTH[fishType]*FISH_LEFT_AR[fishType]), // height
-      FISH_LEFT_ID[fishType], // id
-      FISH_LEFT_FILES[fishType], // image file
-      FISH_LEFT_NAMES[fishType], // name
-      FISH_LEFT_LENGTH[fishType] ); // width
-	
-    return leftFishId;
-  }
-  
-  /**
-   * Utility function to create a right fish id's
-   * @param 	dpth		fish depth
-   * @param		fishType	int for array selection
-   * @return	rightFishId	returns created ObjectId
-   */
-  private ObjectId createRightFishId(int dpth, int fishType) {
-	// Create Right Fish ID
-    ObjectId rightFishId = new ObjectId(
-      dpth, // depth
-      (int)(FISH_RIGHT_LENGTH[fishType]*FISH_RIGHT_AR[fishType]), // height
-      FISH_RIGHT_ID[fishType], // id
-      FISH_RIGHT_FILES[fishType], // image file
-      FISH_RIGHT_NAMES[fishType], // name
-      FISH_RIGHT_LENGTH[fishType] );
-    
-    return rightFishId;
   }
   
   /**
---
+++ src/cisc275/group3/utility/ConstructFish.java
@@ -0,0 +1,84 @@
+package cisc275.group3.utility;
+
+import cisc275.group3.model.sceneobject.BetaFish;
+
+/**
+ * Interface to simplify the construction of fish objects. This
+ * code used to be within SceneBay.java, but was transitioned
+ * into an interface to reduce code size and increase 
+ * extensibility.
+ */
+public interface ConstructFish {
+  // Fish File Locations
+  static final String[] LEFT_FILE = 
+	    {"img/fish_left_1.png", "img/fish_left_2.png", "img/fish_left_3.png"};
+  static final String[] RIGHT_FILE = 
+	    {"img/fish_right_1.png", "img/fish_right_2.png", "img/fish_right_3.png"};
+	  
+  // Fish Names
+  static final String[] LEFT_NAME = 
+    {"Butterflyfish", "Rainbow Cichlid", "Goldfish"};
+  static final String[] RIGHT_NAME = 
+	{"Angelfish", "Threadfin Butterflyfish", "Sergeant Major"};
+	  
+  // Fish Dimensions
+  static final int[] LEFT_LENGTH = {125, 100, 80};
+  static final int[] RIGHT_LENGTH = {100, 120, 85};
+  
+  // Fish Aspect Ratios
+  static final double[] LEFT_AR = {0.6367, 0.7898, 0.5828};
+  static final double[] RIGHT_AR = {0.7047, 0.7453, 0.6};
+	  
+  // Fish ID Numbers
+  static final int[] LEFT_ID = {1, 2, 3};
+  static final int[] RIGHT_ID = {4, 5, 6};
+	  
+  // Fish x-axis Speed
+  static final int[] SPEED = {19, 17, 13};
+  
+  /**
+   * Returns a constructed BetaFish object
+   * @param 	depth	int - fish depth
+   * @param		type	int - fish type [0,2]
+   * @param 	x		double - x-axis location
+   * @param		y		double - y-axis location
+   * @return	BetaFish
+   */
+  static public BetaFish constructLeftFish(int depth, int type, double x, double y) {
+    BetaFish leftFish = new BetaFish(depth, 
+                                     (int)(LEFT_LENGTH[type]*LEFT_AR[type]), // height
+                                     LEFT_ID[type], // id
+                                     LEFT_FILE[type], // image file
+                                     LEFT_NAME[type], // name
+                                     (int)LEFT_LENGTH[type], // width
+                                     x, // x position
+                                     y, // y position
+                                     SPEED[type], // x-axis speed
+                                     0, // y-axis speed
+                                     true); // left moving?
+    return leftFish;
+  }
+  
+  /**
+   * Returns a constructed BetaFish object
+   * @param 	depth	int - fish depth
+   * @param		type	int - fish type [0,2]
+   * @param 	x		double - x-axis location
+   * @param		y		double - y-axis location
+   * @return	BetaFish
+   */
+  static public BetaFish constructRightFish(int depth, int type, double x, double y) {
+    BetaFish rightFish = new BetaFish(depth, 
+	                                 (int)(RIGHT_LENGTH[type]*RIGHT_AR[type]), // height
+                                     RIGHT_ID[type], // id
+                                     RIGHT_FILE[type], // image file
+                                     RIGHT_NAME[type], // name
+                                     (int)RIGHT_LENGTH[type], // width
+                                     x, // x position
+                                     y, // y position
+                                     SPEED[type], // x-axis speed
+                                     0, // y-axis speed
+                                     false); // left moving?
+    return rightFish;
+  }
+}
