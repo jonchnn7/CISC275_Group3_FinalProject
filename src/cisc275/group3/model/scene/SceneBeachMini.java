950fad3b4f869dce5ef0a8b32d6d59b57d9a9996
Scott Fones <scott.fones@gmail.com>
2017-11-06 19:19:27 -0500

    moved crab construction to ConstructCrab.java utility interface

--- src/cisc275/group3/model/scene/SceneBeachMini.java
+++ src/cisc275/group3/model/scene/SceneBeachMini.java
@@ -1,30 +1,10 @@
 package cisc275.group3.model.scene;
 
 import cisc275.group3.model.sceneobject.BetaCrab;
-import cisc275.group3.utility.ObjectId;
+import cisc275.group3.utility.ConstructCrab;
 import cisc275.group3.utility.SceneId;
 
-public class SceneBeachMini extends Scene implements PropertyTimed {
-	
-  // Crab File Location
-  private static final String[] CRAB_FILES = 
-	    {"img/crab_red_icon.png", "img/crab_blue_icon.png"};
-  
-  // Crab Names
-  private static final String[] CRAB_NAMES = 
-    {"Cristmas Island Red Crab", "Atlantic Blue Crab"};
-	  
-  // Crab Width
-  private static final double[] CRAB_WIDTH = {100, 125};
-	  
-  // Crab Aspect Ratios (multiply by width for height)
-  private static final double[] CRAB_AR = {1.442, 0.5988};
-	  
-  // Crab ID Numbers
-  private static final int[] CRAB_ID = {100, 200};
-	  
-  // Crab x-axis Speed
-  private static final int[] CRAB_SPEED = {10, 0};
+public class SceneBeachMini extends Scene implements ConstructCrab, PropertyTimed {
 
   public SceneBeachMini(SceneId mani, boolean click, boolean vis) {
 	super(mani, click, vis);
@@ -47,36 +27,23 @@ public class SceneBeachMini extends Scene implements PropertyTimed {
   @Override
   protected void fillScene() {
     for (int i=0; i<3; i++) {
-      // Create Crab ID
-      ObjectId crabId = new ObjectId(5, // depth
-    		                         CRAB_WIDTH[0]*CRAB_AR[0], // height
-    		                         CRAB_ID[0], // id
-    		                         CRAB_FILES[0], // file name
-    		                         CRAB_NAMES[0], // crab name
-    		                         CRAB_WIDTH[0]); // width
-      
-      sceneItems.add(new BetaCrab(crabId, // crab id
-    	                          0+50, // x location
-    	                          i*200 + manifest.getStartY() + 10, // y location
-    	                          CRAB_SPEED[0]+randGen.nextInt(4), // speed x
-    	                          0)); // speed y
+    	
+      // Add NPC Crab
+      sceneItems.add(ConstructCrab.constructCrab(
+                     5, // depth
+                     0, // type
+                     0+50, // x location 
+                     i*200 + manifest.getStartY() + 10)); // y location
     }
     
     // Player Crab
-    ObjectId crabId = new ObjectId(5, // depth
-                                   CRAB_WIDTH[1]*CRAB_AR[1], // height
-                                   CRAB_ID[1], // id
-                                   CRAB_FILES[1], // file name
-                                   CRAB_NAMES[1], // crab name
-                                   CRAB_WIDTH[1]); // width
-
-    sceneItems.add(new BetaCrab(crabId, // crab id
-                                0+50, // x location
-                                600, // y location
-                                0, // speed x
-                                0)); // speed y
-    
+    sceneItems.add(ConstructCrab.constructCrab(
+                   5, // depth
+                   1, // type
+                   0+50, // x location 
+                   600)); // y location    
   }
+  
   @Override
   public void update() {
     sceneItems.forEach((crab)->{
