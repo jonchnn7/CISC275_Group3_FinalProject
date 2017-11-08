3a762b0f774278524ef493363e424e55a6c6425d
Scott Fones <scott.fones@gmail.com>
2017-11-08 14:22:16 -0500

    update!

--- src/ClickAdventure.java
+++ src/ClickAdventure.java
@@ -1,4 +1,3 @@
-import cisc275.group3.controller.Controller;
 import cisc275.group3.controller.GameController;
 
 
@@ -7,7 +6,6 @@ public class ClickAdventure {
   public static void main(String[] args) {
     javax.swing.SwingUtilities.invokeLater(new Runnable() {
       public void run() {
-        //new Controller(1280, 720);
         new GameController(1280, 720);
       }
     });
