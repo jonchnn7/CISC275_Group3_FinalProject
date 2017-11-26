e30d1d2abb44169ac1a743e73b2e55748b846b2b
Scott Fones <scott.fones@gmail.com>
2017-11-26 18:11:57 -0500

    beginning of SceneBay testing class

---
+++ src/cisc275/group3/testing/SceneBayTest.java
@@ -0,0 +1,11 @@
+package cisc275.group3.testing;
+
+/**
+ * Test class for SceneBay.java properties
+ * and methods not included in the abstract
+ * class Scene.java.
+ * @author Scott
+ */
+public class SceneBayTest {
+
+}
