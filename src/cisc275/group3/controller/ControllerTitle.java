78bf1ed328604f866b0f2aa00f887e8904428ebc
Ryan Boedeker <rboed@udel.edu>
2017-11-27 20:04:51 -0500

    documentation for controllerTitle

--- src/cisc275/group3/controller/ControllerTitle.java
+++ src/cisc275/group3/controller/ControllerTitle.java
@@ -23,8 +23,15 @@ import cisc275.group3.view.GameWindow;
 import cisc275.group3.view.ViewGame;
 
 /**
+ * The Title Controller is responsible for the control of the title screen.
+ * 
+ * The title screen is the first interface that is visible for the user and 
+ * provides a start button that sends the user to the hq upon clicking.
+ * 
+ * ControllerTitle.java
  * 
  * @author Ryan
+ * @author Scott
  */
 
 public class ControllerTitle extends ControllerScene implements LinkDynamics, LinkTime {
@@ -38,20 +45,20 @@ public class ControllerTitle extends ControllerScene implements LinkDynamics, Li
 
 	@Override
 	protected void createScene(int sceneType) {
-	  titlePanel = new JPanel(true) {
-      @Override
-      public void paintComponent(Graphics g) {
-        Dimension size = new Dimension(1280, 720);
-        g.drawImage(BG_IMAGE.getImage(), 0, 0, size.width, size.height, this);
-      }
-    };
-    
-    // Set Properties
-    titlePanel.setLayout(null);
-    titlePanel.setPreferredSize(new Dimension(1280, 720));
-    titlePanel.setBounds(0, 0, 1280, 720);
-    titlePanel.setOpaque(true);
-    
+		titlePanel = new JPanel(true) {
+			@Override
+			public void paintComponent(Graphics g) {
+				Dimension size = new Dimension(1280, 720);
+				g.drawImage(BG_IMAGE.getImage(), 0, 0, size.width, size.height, this);
+			}
+		};
+
+		// Set Properties
+		titlePanel.setLayout(null);
+		titlePanel.setPreferredSize(new Dimension(1280, 720));
+		titlePanel.setBounds(0, 0, 1280, 720);
+		titlePanel.setOpaque(true);
+
 		titlePanel.setName("TitleLayer");
 
 		addTitleButton();
@@ -67,20 +74,22 @@ public class ControllerTitle extends ControllerScene implements LinkDynamics, Li
 		startButton.setBounds(500, 600, 300, 96);
 		startButton.setIcon(new ImageIcon("img/startButton.png"));
 		startButton.setOpaque(false);
-    startButton.setBorderPainted(false);
-    startButton.setBorder(null);
-    startButton.setMargin(new Insets(0, 0, 0, 0));
-    startButton.setContentAreaFilled(false);
-    startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		startButton.setBorderPainted(false);
+		startButton.setBorder(null);
+		startButton.setMargin(new Insets(0, 0, 0, 0));
+		startButton.setContentAreaFilled(false);
+		startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		startButton.setSize(330, 96);
 		startButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			  Component titleComponent = mainPane.getComponentsInLayer(mainPane.getLayer(componentList.get("Title")))[0];
-		    Component hqComponent = mainPane.getComponentsInLayer(mainPane.getLayer(componentList.get("HQ")))[0];
-		    Component missionComponent = mainPane.getComponentsInLayer(mainPane.getLayer(componentList.get("Mission")))[0];
+				Component titleComponent = mainPane
+						.getComponentsInLayer(mainPane.getLayer(componentList.get("Title")))[0];
+				Component hqComponent = mainPane.getComponentsInLayer(mainPane.getLayer(componentList.get("HQ")))[0];
+				Component missionComponent = mainPane
+						.getComponentsInLayer(mainPane.getLayer(componentList.get("Mission")))[0];
 
-				mainPane.setLayer(hqComponent, LayerCode.MainAll.getCode());			
+				mainPane.setLayer(hqComponent, LayerCode.MainAll.getCode());
 				mainPane.setLayer(titleComponent, LayerCode.Title.getCode());
 				mainPane.setLayer(missionComponent, LayerCode.Mission.getCode());
 			}
