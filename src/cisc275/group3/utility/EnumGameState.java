4fe6e838eabd211521e7d67ca7fb7c794b6c61b2
Scott Fones <scott.fones@gmail.com>
2017-12-02 18:08:21 -0500

    updated to reflect int values

--- src/cisc275/group3/utility/EnumGameState.java
+++ src/cisc275/group3/utility/EnumGameState.java
@@ -1,8 +1,8 @@
 package cisc275.group3.utility;
 
 public enum EnumGameState {
-  Title(0), Tutorial(1),
-  Play(2);
+  Title(0), Tutorial(1), IN_Tutorial(2),
+  Play(3), IN_Game(4), EndGame(5);
   
   private final int code;
   
