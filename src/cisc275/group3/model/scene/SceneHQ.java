b25e6e3012b3576a1937dab7b59d3577c03d1e84
jstoup7 <jstoup@udel.edu>
2017-12-04 19:01:47 -0500

    Person leaves AFTER mission is turned in

--- src/cisc275/group3/model/scene/SceneHQ.java
+++ src/cisc275/group3/model/scene/SceneHQ.java
@@ -97,7 +97,7 @@ public class SceneHQ extends Scene {
 			for (SceneObject person : sceneItems) {
 				if ((person.getLocation().getX() < 600) && ((((BetaPerson) person).getStatus()) == 1)) {
 					((BetaPerson) person).setStatus(0);
-				} else if (Scene.getCurrentMission().isDoneMission() && ((((BetaPerson) person).getStatus()) == 0)) {
+				} else if ((Scene.getCurrentMission().getObjectNum() == -5) && ((((BetaPerson) person).getStatus()) == 0)) {
 					((BetaPerson) person).setStatus(-1);
 				}
 				((BetaPerson) person).move();
