c9345d92175b403b220f933a7003a966468223d4
Jon Chann <jonchnn7@gmail.com>
2017-12-05 13:21:08 -0500

    BetaPersonTesting done

--- src/cisc275/group3/testing/ObjectPersonTest.java
+++ src/cisc275/group3/testing/ObjectPersonTest.java
@@ -20,12 +20,54 @@ public class ObjectPersonTest {
    * instances of the test crabs.
    */
   @Before
-  public void createCrab() {
+  public void createPerson() {
     //Person
-	BetaPerson testPerson1 = ConstructPerson.constructPerson(1, 0, 0, 0);
-	BetaPerson testPerson2 = ConstructPerson.constructPerson(2, 1, 200, 0);
-	BetaPerson testPerson3 = ConstructPerson.constructPerson(1, 2, 300, 0);
-	BetaPerson testPerson4 = ConstructPerson.constructPerson(2, 3, 400, 0);
+	testPerson1 = ConstructPerson.constructPerson(1, 0, 100, 0);
+	testPerson2 = ConstructPerson.constructPerson(2, 1, 200, 0);
+	testPerson3 = ConstructPerson.constructPerson(1, 2, 300, 0);
+	testPerson4 = ConstructPerson.constructPerson(2, 3, 400, 0);
+  }
+
+  /**
+   * Tests the move().
+   */
+  @Test
+  public void testMoves() {
+    System.out.println("  Testing move():");
+    
+    testPerson3.setStatus(0);
+    testPerson4.setStatus(-1);
+    
+    testPerson1.move();
+    testPerson2.move();
+    testPerson3.move();
+    testPerson4.move();
+
+    
+    // Persons
+    assertEquals("testPerson1 = 15", 85, testPerson1.getLocation().getX(), 0.2);
+    assertEquals("testPerson2 = 15", 185, testPerson2.getLocation().getX(), 0.2);
+    assertEquals("testPerson3 = 15", 300, testPerson3.getLocation().getX(), 0.2);
+    assertEquals("testPerson4 = 15", 415, testPerson4.getLocation().getX(), 0.2);
+
+  }
+  
+  /**
+   * Tests the get and set status methods.
+   */
+  @Test
+  public void testGetSetStatus() {
+    System.out.println("  Testing getSetStatus():");
+    
+    testPerson3.setStatus(0);
+    testPerson4.setStatus(-1);
+    
+    // Persons
+    assertEquals("testPerson1 = 15", 1, testPerson1.getStatus());
+    assertEquals("testPerson2 = 15", 1, testPerson2.getStatus());
+    assertEquals("testPerson3 = 15", 0, testPerson3.getStatus());
+    assertEquals("testPerson4 = 15", -1, testPerson4.getStatus());
+
   }
   
   /**
@@ -34,7 +76,6 @@ public class ObjectPersonTest {
   @Test
   public void testGetSpeedX() {
     System.out.println("  Testing getSpeedX():");
-    System.out.println("Testing........." +  testPerson1.getSpeedX());
     // Persons
     assertEquals("testPerson1 = 15", 15, testPerson1.getSpeedX(), 0.2);
     assertEquals("testPerson2 = 15", 15, testPerson2.getSpeedX(), 0.2);
@@ -42,67 +83,18 @@ public class ObjectPersonTest {
     assertEquals("testPerson4 = 15", 15, testPerson4.getSpeedX(), 0.2);
 
   }
-//  
-//  /**
-//   * Test the move() method by calling on each
-//   * crab then checking the x location has moved
-//   * appropriately.
-//   */
-//  @Test
-//  public void testMove() {
-//    System.out.println("  Testing move():");
-//    
-//    // Impartial Crabs
-//    System.out.println("    Testing Impartial Crabs:");
-//    testCrab1.move();
-//    testCrab2.move();
-//    assertEquals("Impartial Crab 1 x Location > 0", true, (testCrab1.getLocation().getX() > 0));
-//    assertEquals("Impartial Crab 2 x Location > 200", true, (testCrab2.getLocation().getX() > 200));
-//    
-//    // Left Crabs
-//    System.out.println("    Testing Left Crabs:");
-//    leftCrab1.move();
-//    leftCrab2.move();
-//    assertEquals("Left Crab 1 x Location < 400", true, (leftCrab1.getLocation().getX() < 400));
-//    assertEquals("Left Crab 2 x Location < 600", true, (leftCrab2.getLocation().getX() < 600));
-//
-//    // Right Crabs
-//    System.out.println("    Testing Right Crabs:");
-//    rightCrab1.move();
-//    rightCrab2.move();
-//    assertEquals("Right Crab 1 x Location > 800", true, (rightCrab1.getLocation().getX() > 800));
-//    assertEquals("Right Crab 2 x Location > 1000", true, (rightCrab2.getLocation().getX() > 1000));
-//  }
-//  
-//  /**
-//   * Test the move(double dx) method by calling
-//   * on each crab then checking the x location has
-//   * moved appropriately.
-//   */
-//  @Test
-//  public void testMoveDouble() {
-//    System.out.println("  Testing move(double dx):");
-//    
-//    // Impartial Crabs
-//    System.out.println("    Testing Impartial Crabs:");
-//    testCrab1.move(20);
-//    testCrab2.move(20);
-//    assertEquals("Impartial Crab 1 x Location > 0", true, (testCrab1.getLocation().getX() > 0));
-//    assertEquals("Impartial Crab 2 x Location > 200", true, (testCrab2.getLocation().getX() > 200));
-//    
-//    // Left Crabs
-//    System.out.println("    Testing Left Crabs:");
-//    leftCrab1.move(20);
-//    leftCrab2.move(20);
-//    assertEquals("Left Crab 1 x Location > 400", true, (leftCrab1.getLocation().getX() > 400));
-//    assertEquals("Left Crab 2 x Location > 600", true, (leftCrab2.getLocation().getX() > 600));
-//
-//    // Right Crabs
-//    System.out.println("    Testing Right Crabs:");
-//    rightCrab1.move(20);
-//    rightCrab2.move(20);
-//    assertEquals("Right Crab 1 x Location > 800", true, (rightCrab1.getLocation().getX() > 800));
-//    assertEquals("Right Crab 2 x Location > 1000", true, (rightCrab2.getLocation().getX() > 1000));
-//    
-//  }
+  
+  /**
+   * Tests the get speedY getter.
+   */
+  @Test
+  public void testGetSpeedY() {
+    System.out.println("  Testing getSpeedY():");
+    // Persons
+    assertEquals("testPerson1 = 15", 0, testPerson1.getSpeedY(), 0.2);
+    assertEquals("testPerson2 = 15", 0, testPerson2.getSpeedY(), 0.2);
+    assertEquals("testPerson3 = 15", 0, testPerson3.getSpeedY(), 0.2);
+    assertEquals("testPerson4 = 15", 0, testPerson4.getSpeedY(), 0.2);
+
+  }
 }
--- src/cisc275/group3/testing/TestRunner.java
+++ src/cisc275/group3/testing/TestRunner.java
@@ -7,120 +7,134 @@ import org.junit.runner.notification.Failure;
 /**
  * Central testing class.
  * <p>
- * Runs all testing classes and outputs results and
- * error messages.
+ * Runs all testing classes and outputs results and error messages.
  * 
  * @author Scott
  */
 public class TestRunner {
-  private static Result result;
-  
+	private static Result result;
+
 	public static void main(String[] args) {
-	  abstractSceneObjectTests();
-	  printSeparator();
-	  
-	  objectCrabTests();
-	  printSeparator();
-	  
-	  objectFishTests();
-    printSeparator();
-	  
+		abstractSceneObjectTests();
+		printSeparator();
+
+		objectCrabTests();
+		printSeparator();
+
+		objectFishTests();
+		printSeparator();
+
+		objectPersonTests();
+		printSeparator();
+
 		sceneBayTests();
 		printSeparator();
-		
-		abstractTests(); 
+
+		abstractTests();
 		printSeparator();
 	}
-	
-	 /**
-   * Runs the Generic/Shared tests for
-   * AbstractObjectTests.java
-   */
-  private static void abstractSceneObjectTests() {
-    // Generic Scene Object Tests
-    System.out.println("\nRunning Generic Scene Object Tests...");
-    
-    result = JUnitCore.runClasses(AbstractSceneObjectTests.class);
 
-    for (Failure failure : result.getFailures()) {
-       System.out.println("Failure: " + failure.toString());
-    }
-      
-    System.out.println("Generic Scene Object Tests Successful: " + result.wasSuccessful());
-  } 
-  
-  /**
-  * Runs the tests for ObjectCrab.java
-  */
- private static void objectCrabTests() {
-   // Scene Object Tests
-   System.out.println("\nRunning Object Crab Tests...");
-   
-   result = JUnitCore.runClasses(ObjectCrabTest.class);
-
-   for (Failure failure : result.getFailures()) {
-      System.out.println("Failure: " + failure.toString());
-   }
-     
-   System.out.println("Running Object Crab Tests Successful: " + result.wasSuccessful());
- } 
- 
- /**
- * Runs the tests for ObjectFish.java
- */
-private static void objectFishTests() {
-  // Scene Object Tests
-  System.out.println("\nRunning Object Fish Tests...");
-  
-  result = JUnitCore.runClasses(ObjectFishTest.class);
-
-  for (Failure failure : result.getFailures()) {
-     System.out.println("Failure: " + failure.toString());
-  }
-    
-  System.out.println("Running Object Fish Tests Successful: " + result.wasSuccessful());
-} 
-	
 	/**
-	 * Runs the Bay Specific tests for
-	 * SceneBay.java
+	 * Runs the Generic/Shared tests for AbstractObjectTests.java
+	 */
+	private static void abstractSceneObjectTests() {
+		// Generic Scene Object Tests
+		System.out.println("\nRunning Generic Scene Object Tests...");
+
+		result = JUnitCore.runClasses(AbstractSceneObjectTests.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+
+		System.out.println("Generic Scene Object Tests Successful: " + result.wasSuccessful());
+	}
+
+	/**
+	 * Runs the tests for ObjectCrab.java
+	 */
+	private static void objectCrabTests() {
+		// Scene Object Tests
+		System.out.println("\nRunning Object Crab Tests...");
+
+		result = JUnitCore.runClasses(ObjectCrabTest.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+
+		System.out.println("Running Object Crab Tests Successful: " + result.wasSuccessful());
+	}
+
+	/**
+	 * Runs the tests for ObjectFish.java
+	 */
+	private static void objectFishTests() {
+		// Scene Object Tests
+		System.out.println("\nRunning Object Fish Tests...");
+
+		result = JUnitCore.runClasses(ObjectFishTest.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+
+		System.out.println("Running Object Fish Tests Successful: " + result.wasSuccessful());
+	}
+
+	/**
+	 * Runs the tests for ObjectPerson.java
+	 */
+	private static void objectPersonTests() {
+		// Scene Object Tests
+		System.out.println("\nRunning Object Person Tests...");
+
+		result = JUnitCore.runClasses(ObjectPersonTest.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+
+		System.out.println("Running Object Person Tests Successful: " + result.wasSuccessful());
+	}
+
+	/**
+	 * Runs the Bay Specific tests for SceneBay.java
 	 */
 	private static void sceneBayTests() {
-	  // Bay Specific Tests
-    System.out.println("\nRunning Bay Specific Scene Tests...");
-    
-    result = JUnitCore.runClasses(SceneBayTest.class);
+		// Bay Specific Tests
+		System.out.println("\nRunning Bay Specific Scene Tests...");
 
-    for (Failure failure : result.getFailures()) {
-       System.out.println("Failure: " + failure.toString());
-    }
-      
-    System.out.println("Bay Specific Scene Tests Successful: " + result.wasSuccessful());
-	}	
+		result = JUnitCore.runClasses(SceneBayTest.class);
 
-  /**
-   * Runs the Generic/Shared Abstract
-   * Scene.java tests
-   */
-  private static void abstractTests() {
-    // Abstract Scene Tests
-    System.out.println("\nRunning Generic Scene Tests...");
-      
-    result = JUnitCore.runClasses(AbstractSceneTests.class);
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
 
-    for (Failure failure : result.getFailures()) {
-       System.out.println("Failure: " + failure.toString());
-    }
-      
-    System.out.println("Generic Scene Tests Successful: " + result.wasSuccessful()); 
-  }
-  
+		System.out.println("Bay Specific Scene Tests Successful: " + result.wasSuccessful());
+	}
 
-  /**
-   * Prints a test separator to stdout
-   */
-  private static void printSeparator() {
-    System.out.println("\n\n========================================");
-    System.out.println("========================================\n\n");
-  }
+	/**
+	 * Runs the Generic/Shared Abstract Scene.java tests
+	 */
+	private static void abstractTests() {
+		// Abstract Scene Tests
+		System.out.println("\nRunning Generic Scene Tests...");
+
+		result = JUnitCore.runClasses(AbstractSceneTests.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println("Failure: " + failure.toString());
+		}
+
+		System.out.println("Generic Scene Tests Successful: " + result.wasSuccessful());
+	}
+
+	/**
+	 * Prints a test separator to stdout
+	 */
+	private static void printSeparator() {
+		System.out.println("\n\n========================================");
+		System.out.println("========================================\n\n");
+	}
 }
